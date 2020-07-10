f267db60c46f474ceeb4cbcc29d31f74227acf65
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2020-07-10 15:43:32 -0400

    Updates

---
+++ precache-manifest.586d97d55c843e8575a1fd842ce8b80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3c72d47b7293490358fdeee7a6754b",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "ed9d4c9f26f1d0446140",
+    "url": "/smartbrain/static/css/main.a481e4a4.chunk.css"
+  },
+  {
+    "revision": "3418e6485ee3a08468ac",
+    "url": "/smartbrain/static/js/2.e48f319c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/smartbrain/static/js/2.e48f319c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9d4c9f26f1d0446140",
+    "url": "/smartbrain/static/js/main.6e0292f4.chunk.js"
+  },
+  {
+    "revision": "ae456e4515232efad5b0",
+    "url": "/smartbrain/static/js/runtime-main.b715ec47.js"
+  }
+]);
